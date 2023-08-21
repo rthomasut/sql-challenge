b57c915380fe76cc88ce530170381f2f94c0ada0
Richard Thomas <136641454+rthomasut@users.noreply.github.com>
2023-08-21 12:24:44 -0600

    Update emp_queries.sql

--- EmployeeSQL/emp_queries.sql
+++ EmployeeSQL/emp_queries.sql
@@ -2,8 +2,7 @@
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 LEFT JOIN salaries s
-ON e.emp_no = s.emp_no
-LIMIT 10;
+ON e.emp_no = s.emp_no;
 
 --List the first name, last name, and hire date for the employees who were hired in 1986
 SELECT e.first_name, e.last_name, e.hire_date 
@@ -28,8 +27,7 @@ LEFT JOIN dept_emp de
 ON e.emp_no = de.emp_no
 INNER JOIN departments as d
 ON de.dept_no = d.dept_no
-ORDER BY e.emp_no
-LIMIT 10;
+ORDER BY e.emp_no;
 
 --List first name, last name, and sex of each employee whose first name is Hercules and 
 --whose last name begins with the letter B.
